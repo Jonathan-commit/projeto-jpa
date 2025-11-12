22ec9b7e7378aab222ef2360d4303733a27bd5ab
Jonathan-commit <jonathan_r_schmitt@estudante.sesisenai.org.br>
2025-11-12 20:50:19 -0300

    refactor: remove metodo construtor com Lombok
    Usado o plugin Lombok, para reduzir linhas de código.
    Retirando métodos: Static, vazio, Getter, Setter e ToString

--- src/main/java/com/schmitt/projeto_jpa/dto/DepartamentoDto.java
+++ src/main/java/com/schmitt/projeto_jpa/dto/DepartamentoDto.java
@@ -2,24 +2,36 @@ package com.schmitt.projeto_jpa.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class DepartamentoDto {
 	
 		private int idDepartamento;
 	    @NotBlank (message = "Nome não pode ser vazio")
 	    @Size (max = 50, message = "Nome não pode ultrapassar 50 caracteres")
 		private String nomeDepartamento;
-
+/*
 		public DepartamentoDto(int idDepartamento, String nomeDepartamento) {
 			super();
 			this.idDepartamento = idDepartamento;
 			this.nomeDepartamento = nomeDepartamento;
 		}
-		
+*/
+/*
 		public DepartamentoDto () {  // conversor vazio
 			
 		}
-
+*/
+/*
 		public int getIdDepartamento() {
 			return idDepartamento;
 		}
@@ -35,13 +47,12 @@ public class DepartamentoDto {
 		public void setNomeDepartamento(String nomeDepartamento) {
 			this.nomeDepartamento = nomeDepartamento;
 		}
-
+*/
+/*
 		@Override
 		public String toString() {
 			return "\nDepartamentoDto [idDepartamento=" + idDepartamento + ", nomeDepartamento=" + nomeDepartamento + "]";
 		}
-		
-		
-		
+*/				
 
 }
